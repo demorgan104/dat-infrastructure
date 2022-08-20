c2740d5932cd006b81bebf92f074bc119c4fc4f2
demorgan104 <timotei@tutanota.de>
2022-08-20 17:12:12 +0200

    update data

--- jenkins/data/plugins/lockable-resources/js/lockable-resources.js
+++ jenkins/data/plugins/lockable-resources/js/lockable-resources.js
@@ -11,8 +11,30 @@ function resource_action(button, action) {
   // TODO: Migrate to form:link after Jenkins 2.233 (for button-styled links)
   var form = document.createElement('form');
   form.setAttribute('method', 'POST');
-  form.setAttribute('action', action + "?resource=" + find_resource_name(button));
+  form.setAttribute('action', action + "?resource=" + encodeURIComponent(find_resource_name(button)));
   crumb.appendToForm(form);
   document.body.appendChild(form);
   form.submit();
 }
+
+function replaceNote(element, resourceName) {
+  var d = document.getElementById("note-" + resourceName);
+  $(d).down().next().innerHTML = "<div class='spinner-right' style='flex-grow: 1;'>loading...</div>";
+  new Ajax.Request(
+      "noteForm",
+      {
+        parameters: {resource: resourceName},
+        onComplete : function(x) {
+          d.innerHTML = x.responseText;
+          evalInnerHtmlScripts(x.responseText,function() {
+            Behaviour.applySubtree(d);
+            d.getElementsByTagName("TEXTAREA")[0].focus();
+          });
+          layoutUpdateCallback.call();
+        }
+      }
+  );
+  return false;
+}
+
+
